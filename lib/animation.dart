3400dee177f05f9df98efe97e7ff002584d66a92
Mohil Bansal <mohil1111bansal@gmail.com>
2023-06-06 10:13:22 +0530

    Time Pass

--- lib/animation.dart
+++ lib/animation.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+class AnimationTimePass extends StatelessWidget {
+  const AnimationTimePass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MOHIL BANSAL'),
+      ),
+    );
+  }
+}
